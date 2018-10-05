4635717f1adb903060b2fdcbc3fb274e2ed20f89
Hunt Zhan <huntzhan.dev@gmail.com>
2018-10-06 03:03:13 +0800

    bugfix

---
+++ .travis/hugo_deb.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = 'https://api.github.com/repos/gohugoio/hugo/releases/latest'
+data = requests.get(API_URL).json()
+
+item = list(filter(
+    lambda obj: obj['name'].endswith('Linux-64bit.deb') and 'extended' not in obj['name'],
+    data['assets'],
+))
+assert len(item) == 1
+browser_download_url = item[0]['browser_download_url']
+
+with open('hugo.deb', 'wb') as fout, requests.get(browser_download_url) as rep:
+    fout.write(rep.content)
