1c197d0eb88b60ba480719a1298681765048e774
Hunt Zhan <huntzhan.dev@gmail.com>
2018-10-06 03:27:20 +0800

    bugfix

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,7 +3,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # build site (generate ./public)
 hugo
 # copy ./public to /tmp/hugo-public
-mkdir -p /tmp/hugo-public && cp -r ./public /tmp/hugo-public
+mkdir -p /tmp/hugo-public && cp -r ./public/* /tmp/hugo-public
 cd /tmp/hugo-public
 
-ls -al
+find .
