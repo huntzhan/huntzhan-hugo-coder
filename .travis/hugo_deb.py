e0a71c3e2e623ce10146bfad20ae3bc8736a42ba
Hunt Zhan <huntzhan.dev@gmail.com>
2018-10-06 23:47:51 +0800

    Add logging

--- .travis/hugo_deb.py
+++ .travis/hugo_deb.py
@@ -3,6 +3,9 @@ import requests
 API_URL = 'https://api.github.com/repos/gohugoio/hugo/releases/latest'
 data = requests.get(API_URL).json()
 
+from pprint import pprint
+pprint(data)
+
 item = list(filter(
     lambda obj: obj['name'].endswith('Linux-64bit.deb') and 'extended' not in obj['name'],
     data['assets'],
