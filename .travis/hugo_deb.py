896d33333a87bf0f8455e6f5af9ef94832a03f7b
huntzhan <huntzhan.dev@gmail.com>
2019-08-07 11:16:30 +0800

    Upgrade hugo & theme.

--- .travis/hugo_deb.py
+++ .travis/hugo_deb.py
@@ -1,6 +1,6 @@
 import requests
 
-browser_download_url = 'https://github.com/gohugoio/hugo/releases/download/v0.55.5/hugo_0.55.5_Linux-64bit.deb'
+browser_download_url = 'https://github.com/gohugoio/hugo/releases/download/v0.56.3/hugo_0.56.3_Linux-64bit.deb'
 
 with open('hugo.deb', 'wb') as fout:
     print(browser_download_url)
